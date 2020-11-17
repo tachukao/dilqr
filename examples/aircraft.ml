fabf5221d8e5913a1ae059d32e61b851c0675109
marineschimel <marine.schimel@hotmail.fr>
2020-11-17 11:58:18 +0000

    aircraft changes

--- examples/aircraft.ml
+++ examples/aircraft.ml
@@ -9,7 +9,7 @@ let in_dir = Printf.sprintf "%s/%s" dir
 module P = struct
   let n = 3
   let m = 3
-  let n_steps = 2000
+  let n_steps = 200
   let dt = AD.F 1E-3
   let g = AD.F 9.8
   let mu = AD.F 0.01
@@ -26,22 +26,30 @@ module P = struct
   let alpha = AD.Mat.ones 1 3
 
   let dyn ~theta ~k:_ ~x ~u =
-    ignore theta;
-    let theta = AD.Maths.sqr theta in
-    let __a = AD.Maths.(__a * theta) in
-    let dx = AD.Maths.((x *@ __a) + u) in
+    let theta = AD.Maths.get_slice [ []; [ 9; -1 ] ] theta in
+    let theta = AD.Maths.reshape theta [| 3; 3 |] in
+    let dx = AD.Maths.((x *@ __a) + (u *@ theta)) in
     AD.Maths.(x + (dx * dt))
 
 
-  let dyn_x =
-    Some
+  let dyn_x = None
+
+  (* Some
       (fun ~theta ~k:_ ~x:_ ~u:_ ->
         ignore theta;
         let theta = AD.Maths.sqr theta in
         let __a = AD.Maths.(__a * theta) in
-        AD.Maths.((__a * dt) + AD.Mat.eye n))
+        AD.Maths.((__a * dt) + AD.Mat.eye n)) *)
 
   let fl_x = None
+  let fl_xx = None
+  let l_xx = None
+  let rl_x = None
+  let rl_u = None
+  let rl_uu = None
+  let rl_ux = None
+  let dyn_u = None
+  let rl_xx = None
 
   (* Some
       (fun ~theta ~k:_ ~x ->
@@ -50,14 +58,15 @@ module P = struct
         AD.Maths.(F 0. * theta * x)) *)
 
   let running_loss ~theta ~k:_k ~x ~u =
-    let beta = AD.Maths.get_slice [ []; [ 0; 2 ] ] theta in
-    AD.Maths.(sum' (sqr (x *@ transpose beta)) + sum' (u * alpha * u))
+    let theta = AD.Maths.get_slice [ []; [ 0; 8 ] ] theta in
+    let theta = AD.Maths.reshape theta [| 3; 3 |] in
+    AD.Maths.(sum' (sqr x) + (sum' (sqr theta) * (AD.F 0.1 * sum' (sqr (u *@ theta)))))
 
 
   let final_loss ~theta ~k:_k ~x =
-    let theta = AD.Maths.get_slice [ []; [ 0; 2 ] ] theta in
     let theta = AD.Maths.sqr theta in
-    AD.Maths.(F 1. * sum' (sqr theta) * sum' (x * x))
+    ignore theta;
+    AD.Maths.(AD.F 0. * sum' (x * x))
 end
 
 module M = Dilqr.Default.Make (P)
@@ -68,7 +77,7 @@ let unpack a =
   x0, theta
 
 
-let example () =
+(* let example () =
   let stop prms =
     let x0, theta = unpack prms in
     let cprev = ref 1E9 in
@@ -84,7 +93,7 @@ let example () =
   let f us prms =
     (* let x0, theta = AD.Mat.ones 1 3, prms in *)
     let x0, theta = unpack prms in
-    let fin_taus = M.ilqr ~linesearch:true ~stop:(stop prms) ~us x0 theta in
+    let fin_taus = M.ilqr ~linesearch:true ~stop:(stop prms) x0 theta us in
     let _ =
       Mat.save_txt
         ~out:(in_tmp_dir "taus_ilqr")
@@ -117,8 +126,7 @@ let example () =
       let new_prms = AD.Maths.(prms - (eta * dff)) in
       grad_descent (succ k) new_prms)
   in
-  grad_descent 0 (AD.Maths.reshape P.__b [| 1; 9 |]) |> ignore
-
+  grad_descent 0 (AD.Maths.reshape P.__b [| 1; 9 |]) |> ignore *)
 
 (* problem in the dynamics somewhere, when theta is given the M.ilqr and the loss seem to differ? Maybe one of them doesn't take into account the theta value?*)
 let test_grad () =
@@ -126,7 +134,7 @@ let test_grad () =
   let n_samples = 1 in
   let stop prms =
     (* let x0, theta = prms, AD.Mat.ones 1 3 in *)
-    let x0, theta = unpack prms in
+    let x0, theta = AD.Mat.ones 1 3, prms in
     let cprev = ref 1E9 in
     fun k us ->
       let c = M.loss ~theta x0 us in
@@ -138,14 +146,13 @@ let test_grad () =
       pct_change < 1E-3
   in
   let f us prms =
-    let x0, theta = unpack prms in
-    (* let x0, theta = AD.Mat.ones 1 3, prms in *)
+    let x0, theta = AD.Mat.ones 1 3, prms in
     (* let x0, theta = prms, AD.Mat.ones 1 3 in *)
     let fin_taus = M.ilqr ~linesearch:false ~stop:(stop prms) ~us x0 theta in
     AD.Maths.sum fin_taus
   in
   let ff prms = f (List.init P.n_steps (fun _ -> AD.Mat.zeros 1 P.m)) prms in
-  let samples, directions = FD.generate_test_samples (1, 6) n_samples in
+  let samples, directions = FD.generate_test_samples (1, 18) n_samples in
   let threshold = 1E-5 in
   let eps = 1E-5 in
   let b1, k1 =
@@ -165,8 +172,7 @@ let () =
   (* example (); *)
   test_grad ()
 
-
-(* problem in the dynamics somewhere, when theta is given the M.ilqr and the loss seem to differ? Maybe one of them doesn't take into account the theta value?*)
+(* 
 let test_grad2 () =
   let module FD = Owl_algodiff_check.Make (Algodiff.D) in
   let n_samples = 1 in
@@ -189,7 +195,7 @@ let test_grad2 () =
     let taus, big_fs, big_cs, cs, lambdas = M.g1 ~x0 ~ustars theta in
     AD.Maths.(sum' taus + sum' big_fs + sum' big_cs + sum' cs + sum' lambdas)
   in
-  let samples, directions = FD.generate_test_samples (1, 6) n_samples in
+  let samples, directions = FD.generate_test_samples (1, 9) n_samples in
   let threshold = 1E-5 in
   let eps = 1E-5 in
   let b1, k1 =
@@ -202,4 +208,4 @@ let test_grad2 () =
       ~f:ff
       samples
   in
-  Printf.printf "%b, %i\n%!" b1 k1
+  Printf.printf "%b, %i\n%!" b1 k1 *)
